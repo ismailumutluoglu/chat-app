0500bbcd6858dbc4e0ba3ffc03b483b2c3afc2e8
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-05-04 12:25:58 +0300

    küçük değişiklik

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
 window.__accessToken = null;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
