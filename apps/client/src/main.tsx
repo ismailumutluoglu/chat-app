5c583fe7654dea2e5ea88c26249948e70116d498
İsmail Umutluoğlu <ismailumutluoglu10@gmail.com>
2026-05-04 12:09:59 +0300

    chore(client): initialize window token and remove vite default files

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+window.__accessToken = null;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
